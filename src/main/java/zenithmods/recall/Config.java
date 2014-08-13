de049803003fa9e81e921c64223b2eb25386fb02
Zenith-One <the.number.zero@gmail.com>
2014-08-13 03:56:53 -0400

    Updated version in Config.java

--- src/main/java/zenithmods/recall/Config.java
+++ src/main/java/zenithmods/recall/Config.java
@@ -4,7 +4,7 @@ import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.common.config.Configuration;
 
 public class Config {
-	public static final String VERSION = "0.1.0";
+	public static final String VERSION = "0.0.1";
 	public static final String MODID = "RecallResurrection";
 	public static final String MOD_TITLE = "Recall Resurrection";
 		
